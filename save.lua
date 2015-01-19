140278f4e5dbb80b1e0de55bdb2dbcccea747bf0
strykes <renebvip@hotmail.fr>
2015-01-19 12:24:31 +0100

    Update save.lua

--- save.lua
+++ save.lua
@@ -1,6 +1,6 @@
 PLUGIN.Name = "save"
 PLUGIN.Title = "Save"
-PLUGIN.Version = V(1, 1, 2)
+PLUGIN.Version = V(1, 2, 0)
 PLUGIN.Description = "Manually save your world"
 PLUGIN.Author = "Reneb"
 PLUGIN.HasConfig = true
@@ -21,16 +21,12 @@ function PLUGIN:ccmdSaveAll(arg)
         player = arg.connection.player
     end
 
-    if player and not player:IsAdmin() then
+    if player and ( player:GetComponent("BaseNetworkable").net.connection.authLevel < self.Config.authLevel )  then
         return true
     end
     
     if command == "save.all" then
-		local filename = "0"
-        if arg.Args and arg.Args.Length >= 1 then
-           filename = arg.Args[0]
-        end
-		arg:ReplyWith(tostring(self:SaveAll(filename)))
+		arg:ReplyWith(tostring(self:SaveAll()))
     end
     return
 end
@@ -38,20 +34,13 @@ function PLUGIN:cmdSave( player, com, args )
 	local authlevel = player:GetComponent("BaseNetworkable").net.connection.authLevel
 	local neededlevel = self.Config.authLevel 
 	if(authlevel >= neededlevel) then
-		local filename = "0"
-		if(args.Length >= 1) then
-			filename = args[0]
-		end
-		rust.SendChatMessage(player,tostring(self:SaveAll(filename)))
+		rust.SendChatMessage(player,tostring(self:SaveAll()))
 	end
 end
 
-function PLUGIN:SaveAll(filename)
-	if(not filename) then filename = "0" end
-	local folder = global.server.GetServerFolder("save/".. UnityEngine.Application.get_loadedLevelName() .. "_" .. global.World.get_Size() .. "_" .. global.World.get_Seed() )
-	local fullpath = folder .. "/" .. filename .. ".sav"
-	global.SaveRestore.Save(fullpath)
-	return "File " .. fullpath .. " was Saved" 
+function PLUGIN:SaveAll()
+	global.SaveRestore.Save.methodarray[1]:Invoke(nil, util.TableToArray( { } ))
+	return "World was Saved" 
 end
 
 function PLUGIN:OnRunCommand( arg )
