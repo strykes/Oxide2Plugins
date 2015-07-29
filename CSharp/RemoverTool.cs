3c9c684ead55e72882f8b56a1c8279824011a2e1
strykes <renebvip@hotmail.fr>
2015-07-28 21:56:16 -0400

    Update RemoverTool.cs

--- CSharp/RemoverTool.cs
+++ CSharp/RemoverTool.cs
@@ -1,5 +1,3 @@
-// Reference: Oxide.Ext.Rust
-
 using System.Collections.Generic;
 using System;
 using System.Reflection;
@@ -11,10 +9,10 @@ using Oxide.Core.Plugins;
 
 namespace Oxide.Plugins
 {
-    [Info("RemoverTool", "Reneb & Mughisi & Cryptoc", "2.2.13", ResourceId = 651)]
+    [Info("RemoverTool", "Reneb & Mughisi & Cryptoc", "2.2.17", ResourceId = 651)]
     class RemoverTool : RustPlugin
     {
-        private static DateTime epoch;
+    	private static DateTime epoch;
         private bool Changed;
 
         private int removeAuth;
@@ -28,6 +26,7 @@ namespace Oxide.Plugins
         private decimal refundRate;
         private bool useToolCupboard;
         private bool useRustIO;
+        private bool useGui;
         private string noAccess;
         private string cantRemove;
         private string tooFar;
@@ -36,15 +35,19 @@ namespace Oxide.Plugins
         private string noToolCupboard;
         private string noToolCupboardAccess;
         private string noTargetFound;
-        private string canRemove;
-        private string canRemoveAdmin;
-        private string canRemoveAll;
-        private string canRemoveGive;
+		private string canRemove;
+		private string canRemoveAdmin;
+		private string canRemoveAll;
+		private string canRemoveGive;
         private string helpBasic;
         private string helpAdmin;
         private string helpAll;
         private string helpRay;
-        private MethodInfo isInstalled;
+        private string xmin;
+        private string ymin;
+        private string xmax;
+        private string ymax;
+		private MethodInfo isInstalled;
         private MethodInfo hasFriend;
 
         private Dictionary<BasePlayer, double> deactivationTimer;
@@ -53,11 +56,66 @@ namespace Oxide.Plugins
         private List<BasePlayer> todelete;
         private FieldInfo buildingPrivlidges;
         private FieldInfo serverinput;
-        private Library RustIO;
+		private Library RustIO;
 
-        void Loaded()
+
+        public string json = @"[  
+		                { 
+							""name"": ""RemoveMsg"",
+                            ""parent"": ""Overlay"",
+                            ""components"":
+                            [
+                                {
+                                     ""type"":""UnityEngine.UI.Image"",
+                                     ""color"":""0.1 0.1 0.1 0.7"",
+                                },
+                                {
+                                    ""type"":""RectTransform"",
+                                    ""anchormin"": ""{xmin} {ymin}"",
+                                    ""anchormax"": ""{xmax} {ymax}""
+                                }
+                            ]
+                        },
+						{
+                            ""parent"": ""RemoveMsg"",
+                            ""components"":
+                            [
+                                {
+                                    ""type"":""UnityEngine.UI.Text"",
+                                    ""text"":""{msg}"",
+                                    ""fontSize"":15,
+                                    ""align"": ""MiddleCenter"",
+                                },
+                                {
+                                    ""type"":""RectTransform"",
+                                    ""anchormin"": ""0 0.5"",
+                                    ""anchormax"": ""1 0.9""
+                                }
+                            ]
+                        },
+                        {
+                            ""parent"": ""RemoveMsg"",
+                            ""components"":
+                            [
+                                {
+                                    ""type"":""UnityEngine.UI.Text"",
+                                    ""text"":""{msg2}"",
+                                    ""fontSize"":15,
+                                    ""align"": ""MiddleCenter"",
+                                },
+                                {
+                                    ""type"":""RectTransform"",
+                                    ""anchormin"": ""0 0.1"",
+                                    ""anchormax"": ""1 0.5""
+                                }
+                            ]
+                        }
+                    ]
+                    ";
+
+        void Loaded() 
         {
-            epoch = new System.DateTime(1970, 1, 1);
+        	epoch = new System.DateTime(1970, 1, 1);
             Changed = false;
             deactivationTimer = new Dictionary<BasePlayer, double>();
             removing = new Dictionary<BasePlayer, string>();
@@ -65,39 +123,39 @@ namespace Oxide.Plugins
             nextCheck = CurrentTime() + 1.0;
             buildingPrivlidges = typeof(BasePlayer).GetField("buildingPrivlidges", (BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
             if (!permission.PermissionExists("canRemove")) permission.RegisterPermission("canRemove", this);
-            if (!permission.PermissionExists("canRemoveAdmin")) permission.RegisterPermission("canRemoveAdmin", this);
-            if (!permission.PermissionExists("canRemoveAll")) permission.RegisterPermission("canRemoveAll", this);
-            if (!permission.PermissionExists("canRemoveGive")) permission.RegisterPermission("canRemoveGive", this);
+			if (!permission.PermissionExists("canRemoveAdmin")) permission.RegisterPermission("canRemoveAdmin", this);
+			if (!permission.PermissionExists("canRemoveAll")) permission.RegisterPermission("canRemoveAll", this);
+			if (!permission.PermissionExists("canRemoveGive")) permission.RegisterPermission("canRemoveGive", this);
             serverinput = typeof(BasePlayer).GetField("serverInput", (BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
             LoadVariables();
-            InitializeRustIO();
-
+			InitializeRustIO();
+            json = json.Replace("{xmin}", xmin).Replace("{xmax}", xmax).Replace("{ymin}", ymin).Replace("{ymax}", ymax);
         }
-
-        private void InitializeRustIO() {
-            if(!useRustIO) {
-                RustIO = null;
-                return;
-            }
+		
+		private void InitializeRustIO() {
+			if(!useRustIO) {
+				RustIO = null;
+				return;
+			}
             RustIO = Interface.GetMod().GetLibrary<Library>("RustIO");
             if (RustIO == null || (isInstalled = RustIO.GetFunction("IsInstalled")) == null || (hasFriend = RustIO.GetFunction("HasFriend")) == null) {
                 RustIO = null;
                 Puts("{0}: {1}", Title, "Rust:IO is not present. You need to install Rust:IO first in order to use this plugin!");
             }
         }
-
-        private bool HasFriend(string playerId, string friendId) {
+		
+		private bool HasFriend(string playerId, string friendId) {
             if (RustIO == null) return false;
             return (bool)hasFriend.Invoke(RustIO, new object[] { playerId, friendId });
         }
-        private bool RustIOIsInstalled() {
+		private bool RustIOIsInstalled() {
             if (RustIO == null) return false;
             return (bool)isInstalled.Invoke(RustIO, new object[] {});
         }
-
+		
         void OnServerInitialized()
         {
-
+            
         }
         double CurrentTime()
         {
@@ -127,6 +185,13 @@ namespace Oxide.Plugins
             removeAuth = Convert.ToInt32(GetConfig("Remove", "basic", 0));
             removeAdmin = Convert.ToInt32(GetConfig("Remove", "admin", 2));
             removeAll = Convert.ToInt32(GetConfig("Remove", "all", 2));
+
+            useGui = Convert.ToBoolean(GetConfig("GUI", "activated", true));
+            xmin = Convert.ToString(GetConfig("GUI", "x min", "0.020"));
+            xmax = Convert.ToString(GetConfig("GUI", "x max", "0.20"));
+            ymin = Convert.ToString(GetConfig("GUI", "y min", "0.87"));
+            ymax = Convert.ToString(GetConfig("GUI", "y max", "0.95"));
+
             deactivateTimer = Convert.ToDouble(GetConfig("RemoveTimer", "default", 30));
             deactivateMaxTimer = Convert.ToDouble(GetConfig("RemoveTimer", "max", 120));
             refundAllowed = Convert.ToBoolean(GetConfig("Refund", "activated", true));
@@ -144,16 +209,16 @@ namespace Oxide.Plugins
             noToolCupboardAccess = Convert.ToString(GetConfig("Messages", "noToolCupboardAccess", "You need access to all Tool Cupboards around you to do this"));
             noTargetFound = Convert.ToString(GetConfig("Messages", "noTargetFound", "Target player not found"));
 
-            canRemove = Convert.ToString(GetConfig("permissions", "remove", "canRemove"));
-            canRemoveAdmin = Convert.ToString(GetConfig("permissions", "removeAdmin", "canRemoveAdmin"));
-            canRemoveAll = Convert.ToString(GetConfig("permissions", "removeAll", "canRemoveAll"));
-            canRemoveGive = Convert.ToString(GetConfig("permissions", "removeGive", "canRemoveGive"));
+			canRemove = Convert.ToString(GetConfig("permissions", "remove", "canRemove"));
+			canRemoveAdmin = Convert.ToString(GetConfig("permissions", "removeAdmin", "canRemoveAdmin"));
+			canRemoveAll = Convert.ToString(GetConfig("permissions", "removeAll", "canRemoveAll"));
+			canRemoveGive = Convert.ToString(GetConfig("permissions", "removeGive", "canRemoveGive"));
 
             helpBasic = Convert.ToString(GetConfig("Messages", "helpBasic", "/remove Optional:Time - To remove start removing"));
             helpAdmin = Convert.ToString(GetConfig("Messages", "helpAdmin", "/remove admin Optional:Time - To remove start removing anything"));
             helpAll = Convert.ToString(GetConfig("Messages", "helpAll", "/remove all Optional:Time - To remove start removing an entire building"));
             helpRay = Convert.ToString(GetConfig("Messages", "helpRay", "/rayremove - To remove the entire building that you are looking at"));
-
+            
 
             if (Changed)
             {
@@ -190,7 +255,7 @@ namespace Oxide.Plugins
                             checkFrom.Add(fbuildingblock.transform.position);
                             if (!fbuildingblock.isDestroyed)
                                 fbuildingblock.KillMessage();
-                        }
+                        } 
                         else if (hit.GetComponentInParent<BasePlayer>() == null && hit.GetComponentInParent<BaseEntity>() != null)
                         {
                             if(!(hit.GetComponentInParent<BaseEntity>().isDestroyed))
@@ -316,18 +381,18 @@ namespace Oxide.Plugins
                             return true;
                         if (useRustIO && RustIOIsInstalled())
                         {
-                            if(HasFriend(ownerid, player.userID.ToString()))
-                            {
-                                if (Vector3.Distance(player.transform.position, entity.transform.position) < 3f)
-                                    return true;
-                                else
-                                    SendReply(player, tooFar);
-                            }
-                        }
+                        	if(HasFriend(ownerid, player.userID.ToString()))
+							{
+								if (Vector3.Distance(player.transform.position, entity.transform.position) < 3f)
+									return true;
+								else
+									SendReply(player, tooFar);
+							}
+                        }                    
                     }
                 }
             }
-            if (useToolCupboard)
+			if (useToolCupboard)
             {
                 if (hasTotalAccess(player))
                 {
@@ -343,7 +408,7 @@ namespace Oxide.Plugins
         {
             if (CurrentTime() >= nextCheck)
             {
-                var currenttime = CurrentTime();
+                var currenttime = CurrentTime();                
                 if (removing.Count > 0)
                 {
                     foreach (KeyValuePair<BasePlayer, string> pair in removing)
@@ -354,6 +419,7 @@ namespace Oxide.Plugins
                             double timetodel = deactivationTimer[player];
                             if (currenttime > timetodel)
                                 todelete.Add(player);
+                            LoadMsgGui(player, removing[player], (Math.Floor(timetodel - currenttime)).ToString());
                         }
                         else
                             todelete.Add(player);
@@ -364,6 +430,19 @@ namespace Oxide.Plugins
                 }
                 nextCheck = currenttime + 1;
             }
+        }
+        public void LoadMsgGui(BasePlayer player, string Msg, string Msg2)
+        {
+            if (!useGui) return;
+            var msg = string.Format("Remover Tool ({0})", Msg);
+            var msg2 = string.Format("{0} seconds", Msg2);
+            CommunityEntity.ServerInstance.ClientRPCEx(new Network.SendInfo() { connection = player.net.connection }, null, "DestroyUI", "RemoveMsg");
+            string send = json.Replace("{msg}", msg).Replace("{msg2}", msg2);
+            CommunityEntity.ServerInstance.ClientRPCEx(new Network.SendInfo() { connection = player.net.connection }, null, "AddUI", send);
+        }
+        public void DestroyGui(BasePlayer player)
+        {
+            CommunityEntity.ServerInstance.ClientRPCEx(new Network.SendInfo() { connection = player.net.connection }, null, "DestroyUI", "RemoveMsg");
         }
         void ActivateRemove(BasePlayer player, string ttype, double removetime)
         {
@@ -377,6 +456,7 @@ namespace Oxide.Plugins
             else
                 deactivationTimer.Add(player, (removetime + CurrentTime()));
 
+            LoadMsgGui(player, ttype, removetime.ToString());
             SendReply(player, string.Format("Remover Tool ({0}): Activated for {1} seconds", ttype, removetime.ToString()));
         }
         void DeactivateRemove(BasePlayer player)
@@ -385,8 +465,11 @@ namespace Oxide.Plugins
                 removing.Remove(player);
             if (deactivationTimer.ContainsKey(player))
                 deactivationTimer.Remove(player);
-            if(player.net != null)
+            if (player.net != null)
+            {
                 SendReply(player, "Remover Tool: Deactivated");
+                DestroyGui(player);
+            }
         }
         void TriggerRemove(BasePlayer player, string[] args, string ttype)
         {
@@ -419,15 +502,15 @@ namespace Oxide.Plugins
         }
         bool hasAccess(BasePlayer player, string ttype)
         {
-            string uid = Convert.ToString(player.userID);
+			string uid = Convert.ToString(player.userID);
             if (ttype == "normal" && permission.UserHasPermission(uid, canRemove))
-                return true;
+				return true;
             if (ttype == "admin" && permission.UserHasPermission(uid, canRemoveAdmin))
-                return true;
+				return true;
             if (ttype == "all" && permission.UserHasPermission(uid, canRemoveAll))
-                return true;
+				return true;
             if (ttype == "target" && permission.UserHasPermission(uid, canRemoveGive))
-                return true;
+				return true;
 
             if (ttype == "normal" && player.net.connection.authLevel >= removeAuth)
                 return true;
