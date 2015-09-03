cdd6122682d4e7355f487f482037101024b7f7ea
strykes <renebvip@hotmail.fr>
2015-09-03 23:41:22 +0200

    Update Finder.cs

--- CSharp/Finder.cs
+++ CSharp/Finder.cs
@@ -55,6 +55,23 @@ namespace Oxide.Plugins
 
         void LoadDefaultConfig() { }
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Oxide Hooks
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void Loaded()
+        {
+            lastPositionValue = typeof(BasePlayer).GetField("lastPositionValue", (BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+        void OnServerInitialized()
+        {
+            InitializeTable();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Player Finder
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         object FindPlayer(string arg)
         {
             var findplayers = FindPlayers(arg);
@@ -72,18 +89,18 @@ namespace Oxide.Plugins
             return findplayers;
         }
 
-        Dictionary<string,Dictionary<string, object>> FindPlayers( string arg )
+        Dictionary<string, Dictionary<string, object>> FindPlayers(string arg)
         {
-            var listPlayers = new Dictionary<string,Dictionary<string, object>>();
+            var listPlayers = new Dictionary<string, Dictionary<string, object>>();
 
             ulong steamid = 0L;
             ulong.TryParse(arg, out steamid);
             string lowerarg = arg.ToLower();
 
-            foreach(BasePlayer player in Resources.FindObjectsOfTypeAll<BasePlayer>())
+            foreach (BasePlayer player in Resources.FindObjectsOfTypeAll<BasePlayer>())
             {
-                if(steamid != 0L)
-                    if(player.userID == steamid)
+                if (steamid != 0L)
+                    if (player.userID == steamid)
                     {
                         listPlayers.Clear();
                         listPlayers.Add(steamid.ToString(), GetFinderDataFromPlayer(player));
@@ -96,10 +113,10 @@ namespace Oxide.Plugins
                 }
             }
 
-            if(DeadPlayersList != null)
+            if (DeadPlayersList != null)
             {
                 var deadplayers = DeadPlayersList.Call("GetPlayerList") as Dictionary<string, string>;
-                foreach(KeyValuePair<string, string> pair in deadplayers)
+                foreach (KeyValuePair<string, string> pair in deadplayers)
                 {
                     if (steamid != 0L)
                         if (pair.Key == arg)
@@ -117,10 +134,6 @@ namespace Oxide.Plugins
             }
 
             return listPlayers;
-        }
-        void Loaded()
-        {
-            lastPositionValue = typeof(BasePlayer).GetField("lastPositionValue", (BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
         }
         Dictionary<string, object> GetFinderDataFromDeadPlayers(string userid)
         {
@@ -146,6 +159,10 @@ namespace Oxide.Plugins
 
             return playerData;
         }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Teleport Functions & Data
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         void ResetFind(BasePlayer player)
         {
             if (cachedFind.ContainsKey(player))
@@ -169,6 +186,10 @@ namespace Oxide.Plugins
             lastPositionValue.SetValue(player, player.transform.position);
             player.ClientRPCPlayer(null, player, "ForcePositionTo", new object[] { destination });
         }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Building Privilege Search
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         Dictionary<string,object> GetBuildingPrivilegeData(BuildingPrivlidge bp)
         {
             var bpdata = new Dictionary<string, object>();
@@ -194,12 +215,90 @@ namespace Oxide.Plugins
 
             return privileges;
         }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Sleeping Bag Search
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Dictionary<string, object> GetSleepingBagData(SleepingBag bag)
+        {
+            var bagdata = new Dictionary<string, object>();
+
+            bagdata.Add("name", bag.niceName);
+            bagdata.Add("pos", bag.transform.position);
+
+            return bagdata;
+        }
+        List<Dictionary<string, object>> FindSleepingBags(string userid)
+        {
+            var bags = new List<Dictionary<string, object>>();
+            ulong ulongid = ulong.Parse(userid);
+            foreach(SleepingBag bag in SleepingBag.FindForPlayer(ulongid, true))
+            {
+                bags.Add(GetSleepingBagData(bag));
+            }
+
+            return bags;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Item Search
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Dictionary<string, string> displaynameToShortname = new Dictionary<string, string>();
+
+        private void InitializeTable()
+        {
+            displaynameToShortname.Clear();
+            List<ItemDefinition> ItemsDefinition = ItemManager.GetItemDefinitions() as List<ItemDefinition>;
+            foreach (ItemDefinition itemdef in ItemsDefinition)
+            {
+                displaynameToShortname.Add(itemdef.displayName.english.ToString().ToLower(), itemdef.shortname.ToString());
+            }
+        }
+
+        List<Dictionary<string, object>> FindItems(ItemDefinition itemdef, int minamount)
+        {
+            var containers = new List<Dictionary<string, object>>();
+            foreach (StorageContainer sc in Resources.FindObjectsOfTypeAll<StorageContainer>())
+            {
+                ItemContainer inventory = sc.inventory;
+                if (inventory == null) continue;
+                int amount = inventory.GetAmount(itemdef.itemid, false);
+                if (amount < minamount) continue;
+                Dictionary<string, object> scdata = new Dictionary<string, object>();
+                scdata.Add("in", "Storage Box");
+                scdata.Add("pos", sc.transform.position);
+                scdata.Add("amount", amount.ToString());
+                containers.Add(scdata);
+            }
+
+            foreach (BasePlayer player in Resources.FindObjectsOfTypeAll<BasePlayer>())
+            {
+                PlayerInventory inventory = player.inventory;
+                if (inventory == null) continue;
+                int amount = inventory.GetAmount(itemdef.itemid);
+                if (amount < minamount) continue;
+                Dictionary<string, object> scdata = new Dictionary<string, object>();
+                scdata.Add("in", string.Format("{0} {1}", player.userID.ToString(), player.displayName));
+                scdata.Add("pos", player.transform.position);
+                scdata.Add("amount", amount.ToString());
+                containers.Add(scdata);
+            }
+            return containers;
+        }
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Chat Command
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         [ChatCommand("find")]
         void cmdChatFind(BasePlayer player, string command, string[] args)
         {
             if(args.Length < 2 || args == null)
             {
+                SendReply(player, "======== FINDER =======");
                 SendReply(player, "/find player NAME/SteamID");
+                SendReply(player, "/find bag NAME/SteamID");
+                SendReply(player, "/find privilege NAME/SteamID");
                 SendReply(player, "/find tp FINDID");
                 return;
             }
@@ -209,14 +308,12 @@ namespace Oxide.Plugins
                 case "players":
                 case "player":
                     ResetFind(player);
-
                     Dictionary<string,Dictionary<string, object>> success = FindPlayers(args[1]);
                     if(success.Count == 0)
                     {
                         SendReply(player, string.Format("No players found with: {0}", args[1]));
                         return;
                     }
-
                     
                     foreach(KeyValuePair<string, Dictionary<string,object>> pair in success)
                     {
@@ -225,38 +322,7 @@ namespace Oxide.Plugins
                         count++;
                     }
                 break;
-                case "privilege":
-                case "cupboard":
-                case "toolcupboard":
-                    object successs = FindPlayer(args[1]);
-                    if (successs is string)
-                    {
-                        SendReply(player, (string)successs);
-                        return;
-                    }
-                    if(successs is Dictionary<string, Dictionary<string,object>>)
-                    {
-                        SendReply(player, "Multiple players found, use the SteamID or use a fuller name");
-                        foreach (KeyValuePair<string, Dictionary<string, object>> pair in (Dictionary<string, Dictionary<string, object>>)successs)
-                        {
-                            SendReply(player, string.Format("{0} - {1} - {2} - {3}", pair.Key, (string)pair.Value["name"], (string)pair.Value["state"], (string)pair.Value["status"]));
-                        }
-                        return;
-                    }
-                    ResetFind(player);
-                    List<Dictionary<string, object>> privileges = FindPrivileges((string)((Dictionary<string, object>)successs)["userid"]);
-                    if(privileges.Count == 0)
-                    {
-                        SendReply(player, "No tool cupboard privileges found for this player");
-                        return;
-                    }
-                    foreach(Dictionary<string,object> priv in privileges)
-                    {
-                        AddFind(player, count, priv);
-                        SendReply(player, string.Format("{0} - {1}", count.ToString(), priv["pos"].ToString()));
-                        count++;
-                    }
-                    break;
+
                 case "tp":
                     object finddatar = GetFind(player, args[1]);
                     if(finddatar is string)
@@ -276,6 +342,97 @@ namespace Oxide.Plugins
                         SendReply(player, string.Format("{0} - {1}", pair.Key, pair.Value.ToString()));
                     }
                 break;
+                case "item":
+                    if (args.Length < 3)
+                    {
+                        SendReply(player, "/find item ITEMNAME MINNUMBER");
+                        return;
+                    }
+                    int minnum = 0;
+                    if (!int.TryParse(args[2], out minnum))
+                    {
+                        SendReply(player, "/find item ITEMNAME MINNUMBER");
+                        return;
+                    }
+                    ResetFind(player);
+                    string itemname = args[1].ToLower();
+                    if (displaynameToShortname.ContainsKey(itemname))
+                        itemname = displaynameToShortname[itemname];
+                    ItemDefinition definition = ItemManager.FindItemDefinition(itemname);
+                    if (definition == null)
+                    {
+                        SendReply(player, "This item doesn't exist");
+                        return;
+                    }
+                    List<Dictionary<string, object>> containers = FindItems(definition, minnum);
+                    foreach (Dictionary<string, object> container in containers)
+                    {
+                        AddFind(player, count, container);
+                        SendReply(player, string.Format("{0} - {1} - Amount: {2} - In: {3}", count.ToString(), container["pos"].ToString(), container["amount"], container["in"]));
+                        count++;
+                    }
+                    break;
+                default:
+                    object successs = FindPlayer(args[1]);
+                    if (successs is string)
+                    {
+                        SendReply(player, (string)successs);
+                        return;
+                    }
+                    if (successs is Dictionary<string, Dictionary<string, object>>)
+                    {
+                        SendReply(player, "Multiple players found, use the SteamID or use a fuller name");
+                        foreach (KeyValuePair<string, Dictionary<string, object>> pair in (Dictionary<string, Dictionary<string, object>>)successs)
+                        {
+                            SendReply(player, string.Format("{0} - {1} - {2} - {3}", pair.Key, (string)pair.Value["name"], (string)pair.Value["state"], (string)pair.Value["status"]));
+                        }
+                        return;
+                    }
+                    ResetFind(player);
+                    switch (args[0].ToLower())
+                    {
+                        case "sleepingbag":
+                        case "bag":
+                            List<Dictionary<string, object>> bags = FindSleepingBags((string)((Dictionary<string, object>)successs)["userid"]);
+                            if (bags.Count == 0)
+                            {
+                                SendReply(player, "No sleeping bags found for this player");
+                                return;
+                            }
+                            foreach (Dictionary<string, object> bag in bags)
+                            {
+                                AddFind(player, count, bag);
+                                SendReply(player, string.Format("{0} - {1} - {2}", count.ToString(), bag["name"].ToString(), bag["pos"].ToString()));
+                                count++;
+                            }
+                            break;
+                        case "privilege":
+                        case "cupboard":
+                        case "toolcupboard":
+
+                            List<Dictionary<string, object>> privileges = FindPrivileges((string)((Dictionary<string, object>)successs)["userid"]);
+                            if (privileges.Count == 0)
+                            {
+                                SendReply(player, "No tool cupboard privileges found for this player");
+                                return;
+                            }
+                            foreach (Dictionary<string, object> priv in privileges)
+                            {
+                                AddFind(player, count, priv);
+                                SendReply(player, string.Format("{0} - {1}", count.ToString(), priv["pos"].ToString()));
+                                count++;
+                            }
+                        break;
+                        
+                        default:
+
+
+                            break;
+
+
+                    }
+
+                    break;
             }
         }
     }
